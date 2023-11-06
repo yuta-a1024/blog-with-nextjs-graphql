50ddcb134df400f37d2763610e9512ea585d80e8
渥美雄太 <atsumiyuuta@ayMacBook-Air.local>
2023-11-06 17:57:14 +0900

    img

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -5,16 +5,16 @@ import styles from "../styles/BlogCard.module.css";
 const BlogCard = ({ photo, title, author, date, slug }) => {
   return (
     <div className={styles.card}>
-      <Link href={`/posts/${slug}`}>
+      <Link href={"/posts/" + slug}>
         <div className={styles.imgContainer}>
-          <img src={photo.url} alt='' />
+          <img src={photo.url} alt="" />
         </div>
       </Link>
       <div className={styles.text}>
         <h2>{title}</h2>
         <div className={styles.details}>
           <div className={styles.author}>
-            <img src={author.avatar.url} alt='' />
+            <img src={author.avatar.url} alt="" />
             <h3>{author.name}</h3>
           </div>
           <div className={styles.date}>
